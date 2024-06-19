16808b5318a45f25aa743eedd5e844871f2c1b07
Veikka <veikka.nevala@outlook.com>
2024-06-20 01:41:42 +0300

    fixed canvasRef issues

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -1,67 +1,66 @@
-import React, { useContext, useState } from "react"
+import React, { useContext, useState, useRef } from "react"
 import SocketContext from "../util/socketContext"
 import { useParams } from "react-router-dom"
 
 
 function Canvas() {
     const [mDown, setMDown] = useState(false)
-    const canvasRef = React.createRef()
+    const canvasRef = useRef(null)
     const socket = useContext(SocketContext)
     const room = useParams().room
 
-    const paint = (ctx, x, y) => {
+    const paint = (x, y) => {
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext("2d")
+        console.log(`painting x: ${x} + y: ${y}`)
         ctx.lineTo(x, y)
         ctx.stroke()
     }
 
-    socket.on("startDraw", (id, x, y) => {
-        if(id !== socket.id) {
-            console.log("drawing...")
-            const canvas = this.canvasRef.current
-            const ctx = canvas.getContext("2d")
-            ctx.moveTo(x, y)
-            ctx.fillRect(x, y, 1, 1) //TODO change to variable
-        }
+    socket.on("startDraw", (data) => {
+            ctx.moveTo(data.x, data.y)
+            ctx.fillRect(data.x, data.y, 1, 1) //TODO change to variable
     })
 
-    socket.on("draw", (id, x, y) => {
-        if(id !== socket.id) {
-            console.log("drawing...")
-            const canvas = this.canvasRef.current
-            const ctx = canvas.getContext("2d")
-            paint(ctx, x, y)
-        }
-        
+    socket.on("draw", (data) => {
+        paint(data.x, data.y)
     })
+    
+    
 
     const mouseUp = () => {
         setMDown(false)
     }
 
     const getCanvas = (event) => {
-        const canvas = this.canvasRef.current
-        const ctx = canvas.getContext("2d")
+        const canvas = canvasRef.current
         const {left, top} = canvas.getBoundingClientRect()
         const x = event.clientX - left
         const y = event.clientY - top
-        return {ctx, x, y}
+        return {x, y}
     }
 
     const mouseDown = (event) => {
-        const {ctx, x, y} = getCanvas(event)
+        const {x, y} = getCanvas(event)
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext("2d")
         ctx.moveTo(x, y) 
         ctx.fillRect(x, y, 1, 1) //TODO change to variable
         setMDown(true)
+        
         socket.emit("canvas:startdraw", 
-            ({room: room, id: socket.id, x: x, y: y}))
+            ({room: room, x: x, y: y}))
+            
     }
 
     const mouseMove = (event) => {
-        const {ctx, x, y} = getCanvas(event)
+        const {x, y} = getCanvas(event)
         if(mDown) {
-            paint(ctx, x, y)
+            paint(x, y)
+            
             socket.emit("canvas:draw", 
                 ({room: room, id: socket.id, x: x, y: y}))
+                
             //console.log(event)
         }
     }
--- server/handlers/canvasHandler.js
+++ server/handlers/canvasHandler.js
@@ -1,12 +1,13 @@
 module.exports = (io, socket) => {
-    const startDraw = ({room, id, x, y}) => {
+    const startDraw = ({room, x, y}) => {
         console.log("startDraw")
-        io.to(room).emit("startDraw", (id, x, y))
+        socket.to(room).emit("startDraw", {x: x, y: y})
     }
 
-    const draw = ({room, id, x, y}) => {
+    const draw = ({room, x, y}) => {
         console.log("draw " + room)
-        io.to(room).emit("draw", (id, x, y))
+        console.log(`x: ${x} + y: ${y}`)
+        socket.to(room).emit("draw", {x: x, y: y})
     }
 
     socket.on("canvas:startdraw", startDraw)
